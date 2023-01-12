8662f3509ac6a8cd1c3e427e3d6ba4c184c6d269
karma_os <gusangmo0605@gmail.com>
2023-01-12 20:45:27 +0900

    Update app.py

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model -github action docker_v9 Deploy")
+    st.title("Mask Classification Model -github action docker_v10 Deploy")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
